91a3b4eb6b278e38a0bae743c04ab8c8e9f1ffe1
Julik <lilvolkj@gmail.com>
2022-12-25 23:04:02 +0300

    Cleared up program.cs

---
+++ BeSmart.WebApi/Extensions/ConfigureServicesExtension.cs
@@ -0,0 +1,25 @@
+﻿using BeSmart.Domain.Interfaces;
+using BeSmart.Persistence.Data;
+using BeSmart.Persistence.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace BeSmart.WebApi.Extensions;
+
+public static class ConfigureServicesExtension
+{
+    public static IServiceCollection ConfigureServices(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        services.AddDbContext<DataContext>(options =>
+        {
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            options.UseNpgsql(connectionString, opt =>
+            {
+                opt.MigrationsAssembly("BeSmart.WebApi");
+            });
+        });
+
+        return services;
+    }
+}
--- BeSmart.WebApi/Program.cs
+++ BeSmart.WebApi/Program.cs
@@ -1,27 +1,8 @@
-using BeSmart.Domain;
-using BeSmart.Domain.Interfaces;
-using BeSmart.Persistence;
-using BeSmart.Persistence.Repositories;
-using Microsoft.EntityFrameworkCore;
-
-using BeSmart.Persistence.Data;
-using Microsoft.EntityFrameworkCore;
+using BeSmart.WebApi.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
-
-builder.Services.AddDbContext<BeSmartDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-
-builder.Services.AddDbContext<DataContext>(options =>
-{
-    var connectionString = builder.Configuration.GetConnectionString("default");
-    options.UseSqlite(connectionString, opt =>
-    {
-        opt.MigrationsAssembly("BeSmart.WebApi");
-    });
-});
+builder.Services.ConfigureServices(builder.Configuration);
 
 var app = builder.Build();
 
