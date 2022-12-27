a52512bbaa3356b8d5b6bece91a2bba59a8bec4d
gfady <dashyk130903@gmail.com>
2022-12-27 13:50:38 +0300

    Add AnswerService

--- BeSmart.Application/Interfaces/IServiceBase.cs
+++ BeSmart.Application/Interfaces/IServiceBase.cs
@@ -9,9 +9,9 @@ namespace BeSmart.Application.Interfaces
     public interface IServiceBase<TEntity>
     {
         Task<List<TEntity>> GetAllAsync();
-        Task<TEntity> FindByIdAsync(int Id);
+        Task<TEntity> FindByIdAsync(int id);
         Task<TEntity> AddAsync(TEntity entity);
         Task<TEntity> UpdateAsync(TEntity entity);
-        Task<TEntity> DeleteAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(int id);
     }
 }
---
+++ BeSmart.Application/Service/AnswerService.cs
@@ -0,0 +1,45 @@
+﻿using BeSmart.Application.Interfaces;
+using BeSmart.Domain.Interfaces;
+using BeSmart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSmart.Application.Service
+{
+    public class AnswerService : IServiceAnswer
+    {
+        private readonly IRepositoryManager repoManager;
+        public AnswerService(IRepositoryManager repoManager)
+        {
+            this.repoManager = repoManager;
+        }
+
+        public async Task<List<Answer>> GetAllAsync()
+        {
+            return await repoManager.Answer.GetAllAsync();
+        }
+
+        public async Task<Answer> FindByIdAsync(int id)
+        {
+            return await repoManager.Answer.GetAsync(id);
+        }
+
+        public async Task<Answer> AddAsync(Answer entity)
+        {
+             return await repoManager.Answer.AddAsync(entity);
+        }
+
+        public async Task<Answer> UpdateAsync(Answer entity)
+        {
+            return await repoManager.Answer.UpdateAsync(entity);
+        }
+
+        public async Task<Answer> DeleteAsync(int id)
+        {
+            return await repoManager.Answer.DeleteAsync(id);
+        }
+    }
+}
