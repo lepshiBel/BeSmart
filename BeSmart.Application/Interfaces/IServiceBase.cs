cc66cb51a6420abf60c1b66bb7d7ffa8f9d6ce52
gfady <dashyk130903@gmail.com>
2022-12-27 12:57:32 +0300

    Add IServiceAnswer, IServiceQuestion

---
+++ BeSmart.Application/Interfaces/IServiceBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSmart.Application.Interfaces
+{
+    public interface IServiceBase<TEntity>
+    {
+        Task<List<TEntity>> GetAllAsync();
+        Task<TEntity> FindByIdAsync(int Id);
+        Task<TEntity> AddAsync(TEntity entity);
+        Task<TEntity> UpdateAsync(TEntity entity);
+        Task<TEntity> DeleteAsync(TEntity entity);
+    }
+}
---
+++ BeSmart.Application/Interfaces/IServiceQuestion.cs
@@ -0,0 +1,13 @@
+﻿using BeSmart.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeSmart.Application.Interfaces
+{
+    public interface IServiceQuestion : IServiceBase<Question>
+    {
+    }
+}
