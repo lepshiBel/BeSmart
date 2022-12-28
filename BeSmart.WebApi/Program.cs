452288ea74620b0bf682774f676fc7121e68fdc4
avvakumovelisey <avvakumovelisey@gmail.com>
2022-12-28 15:47:00 +0300

    Added Swagger

--- BeSmart.WebApi/Program.cs
+++ BeSmart.WebApi/Program.cs
@@ -9,6 +9,10 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 builder.Services.AddScoped<IServiceAnswer, AnswerService>();
 builder.Services.AddScoped<IServiceQuestion, QuestionService>();
@@ -18,5 +22,14 @@ builder.Services.AddDbContext<BeSmartDbContext>(options => options.UseNpgsql(bui
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+    options.DocumentTitle = "Swagger";
+});
 
 app.Run();
