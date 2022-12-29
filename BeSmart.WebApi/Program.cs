0d3a0fa4a245bd6a1e999218f244dc6dc25c7848
gfady <dashyk130903@gmail.com>
2022-12-29 12:48:41 +0300

    Removed unnecessary DefaultConnection string and replaced BeSmartDbContext

--- BeSmart.WebApi/Program.cs
+++ BeSmart.WebApi/Program.cs
@@ -22,8 +22,6 @@ builder.Services.AddScoped<IRepositoryManager, RepositoryManager>();
 builder.Services.AddScoped<IServiceAnswer, AnswerService>();
 builder.Services.AddScoped<IServiceQuestion, QuestionService>();
 
-builder.Services.AddDbContext<BeSmartDbContext>(options => options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
-
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
