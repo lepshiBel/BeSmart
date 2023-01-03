4b57cc4cb3c284c42f54509c52b026783f829625
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-03 20:24:57 +0300

    Added CategoryService.cs

---
+++ BeSmart.Application/Service/CategoryService.cs
@@ -0,0 +1,33 @@
+﻿using BeSmart.Application.Interfaces;
+using BeSmart.Domain.Interfaces;
+using BeSmart.Domain.Models;
+using FluentValidation;
+
+namespace BeSmart.Application.Service
+{
+    public class CategoryService : IServiceCategory
+    {
+        private readonly IRepositoryManager repoManager;
+        private readonly IValidator<Category> validator;
+        public CategoryService(IRepositoryManager repoManager, IValidator<Category> validator)
+        {
+            this.repoManager = repoManager;
+            this.validator = validator;
+        }
+
+        public async Task<List<Category>> GetAllCategoriesAsync()
+        {
+            return await repoManager.Category.GetAllAsync();
+        }
+
+        public async Task<Category> FindCategoryByIdAsync(int id)
+        {
+            return await repoManager.Category.GetAsync(id);
+        }
+
+        public async Task<Category> AddCategoryAsync(Category entity)
+        {
+            return await repoManager.Category.AddAsync(entity);
+        }
+    }
+}
