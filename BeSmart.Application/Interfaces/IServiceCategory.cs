1a6a2fbe55eebce5708338042b5617d53a5b50c6
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-03 19:48:40 +0300

    Add IServiceCategory interface

---
+++ BeSmart.Application/Interfaces/IServiceCategory.cs
@@ -0,0 +1,11 @@
+﻿using BeSmart.Domain.Models;
+
+namespace BeSmart.Application.Interfaces
+{
+    public interface IServiceCategory
+    {
+        Task<List<Category>> GetAllCategoriesAsync();
+        Task<Category> FindCategoryByIdAsync(int id);
+        Task<Category> AddCategoryAsync(Category category);
+    }
+}
