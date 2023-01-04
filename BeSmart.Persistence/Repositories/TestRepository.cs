27980b57cb0623eefa0c90a1cd1d5c72021f32c2
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-04 14:27:51 +0300

    Added TestRepository.cs

---
+++ BeSmart.Persistence/Repositories/TestRepository.cs
@@ -0,0 +1,19 @@
+﻿using BeSmart.Domain;
+using BeSmart.Domain.Interfaces;
+using BeSmart.Domain.Models;
+using BeSmart.Persistence.Data;
+
+namespace BeSmart.Persistence.Repositories
+{
+    public class TestRepository : RepositoryBase<Test, BeSmartDbContext>, ITestRepository
+    {
+        public TestRepository(BeSmartDbContext dbContext) : base(dbContext) { }
+
+        public async Task<Test> GetTestWithQuestionsAsync(int id)
+        {
+            var test = await context.Tests.FindAsync(id);
+            await context.Entry(test).Collection(q => q.Questsions).LoadAsync();
+            return test;
+        }
+    }
+}
