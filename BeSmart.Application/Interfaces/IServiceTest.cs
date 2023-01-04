82e9aebf7ff3ea5614c229ff356289013e439de6
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-04 14:05:33 +0300

    Added IServiceTest.cs interface

---
+++ BeSmart.Application/Interfaces/IServiceTest.cs
@@ -0,0 +1,14 @@
+﻿using BeSmart.Domain.Models;
+
+namespace BeSmart.Application.Interfaces
+{
+    public interface IServiceTest
+    {
+        Task<List<Test>> GetAllTestsAsync();
+        Task<Test> FindTestByIdAsync(int id);
+        Task<Test> GetTestWithQuestionsAsync(int id);
+        Task<Test> AddTestAsync(Test test);
+        Task<Test> UpdateTestAsync(Test test);
+        Task<Test> DeleteTestAsync(int id);
+    }
+}
