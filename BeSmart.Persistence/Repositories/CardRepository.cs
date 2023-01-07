6880f029d962128ef8e78bda83eea5c2b10a0062
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-07 18:29:19 +0300

    Added CardRepository.cs

---
+++ BeSmart.Persistence/Repositories/CardRepository.cs
@@ -0,0 +1,15 @@
+﻿using BeSmart.Domain;
+using BeSmart.Domain.Interfaces;
+using BeSmart.Domain.Models;
+using BeSmart.Persistence.Data;
+
+namespace BeSmart.Persistence.Repositories
+{
+    public class CardRepository : RepositoryBase<Card, BeSmartDbContext>, ICardRepository
+    {
+        public CardRepository(BeSmartDbContext dbContext)
+            : base(dbContext)
+        {
+        }
+    }
+}
