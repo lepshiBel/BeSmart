4aa897e30f4656615f730a2fb26219ec78762866
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-07 17:46:06 +0300

    Added dto for card model

---
+++ BeSmart.Domain/DTOs/Card/CardCreationDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace BeSmart.Domain.DTOs.Card
+{
+    public class CardCreationDTO
+    {
+        public string Word { get; set; }
+
+        public string? Text { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? Transctipt { get; set; }
+    }
+}
---
+++ BeSmart.Domain/DTOs/Card/CardDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace BeSmart.Domain.DTOs.Card
+{
+    public class CardDTO
+    {
+        public int Id { get; set; }
+
+        public string Word { get; set; }
+
+        public string? Text { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string? Transctipt { get; set; }
+    }
+}
