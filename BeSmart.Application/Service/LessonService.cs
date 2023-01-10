0908f1ef5d3a80d227f990fabc487b339de38a9f
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-10 12:16:45 +0300

    Added LessonService.cs

---
+++ BeSmart.Application/Service/LessonService.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using BeSmart.Application.Interfaces;
+using BeSmart.Domain.DTOs.Lesson;
+using BeSmart.Domain.Interfaces;
+using BeSmart.Domain.Models;
+
+namespace BeSmart.Application.Service
+{
+    public class LessonService : IServiceLesson
+    {
+        private readonly IRepositoryManager repoManager;
+        private readonly IMapper mapper;
+
+        public LessonService(IRepositoryManager repoManager, IMapper mapper)
+        {
+            this.repoManager = repoManager;
+            this.mapper = mapper;
+        }
+
+        public async Task<List<LessonDTO>> GetAllLessonsAsync()
+        {
+            var lessons = await repoManager.Lesson.GetAllAsync();
+            return lessons == null ? null : mapper.Map<List<LessonDTO>>(lessons);
+        }
+
+        public async Task<LessonDTO> FindLessonByIdAsync(int id)
+        {
+            var lesson = await repoManager.Lesson.GetAllAsync();
+            return lesson == null ? null : mapper.Map<LessonDTO>(lesson);
+        }
+
+        public async Task<LessonWithCardsDTO> GetLessonWithCardsAsync(int id)
+        {
+            var lessonWithCards = await repoManager.Lesson.GetLessonWithCardsAsync(id);
+            return lessonWithCards == null ? null : mapper.Map<LessonWithCardsDTO>(lessonWithCards);
+        }
+
+        public async Task<LessonDTO> AddLessonAsync(LessonCreationDTO lessonCreationDto)
+        {
+            var lessonToCreate = mapper.Map<Lesson>(lessonCreationDto);
+            var createdLesson = await repoManager.Lesson.AddAsync(lessonToCreate);
+            return createdLesson == null ? null : mapper.Map<LessonDTO>(createdLesson);
+        }
+
+        //public async Task<Lesson> UpdateLessonAsync(Lesson lesson)
+        //{
+
+        //    return await repoManager.Lesson.UpdateAsync(lesson);
+        //}
+
+        public async Task<Lesson> DeleteLessonAsync(int id)
+        {
+            return await repoManager.Lesson.DeleteAsync(id);
+        }
+    }
+}
