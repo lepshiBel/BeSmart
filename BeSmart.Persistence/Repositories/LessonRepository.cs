d942b4adb417357064214cd33b03880b42a4a437
avvakumovelisey <avvakumovelisey@gmail.com>
2023-01-10 12:21:17 +0300

    Added LessonRepository.cs

---
+++ BeSmart.Persistence/Repositories/LessonRepository.cs
@@ -0,0 +1,19 @@
+﻿using BeSmart.Domain.Interfaces;
+using BeSmart.Domain.Models;
+using BeSmart.Domain;
+using BeSmart.Persistence.Data;
+
+namespace BeSmart.Persistence.Repositories
+{
+    public class LessonRepository : RepositoryBase<Lesson, BeSmartDbContext>, ILessonRepository
+    {
+        public LessonRepository(BeSmartDbContext dbContext) : base(dbContext) { }
+
+        public async Task<Lesson> GetLessonWithCardsAsync(int id)
+        {
+            var lesson = await context.Lessons.FindAsync(id);
+            await context.Entry(lesson).Collection(q => q.Cards).LoadAsync();
+            return lesson;
+        }
+    }
+}
