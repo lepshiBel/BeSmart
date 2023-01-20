5206a30e66e82d6793a4d6b4e74fcb06990b967b
Margarita Krivosheeva <mkrivosheeva71@gmail.com>
2023-01-20 21:40:11 +0300

    Added MembershipDTO

---
+++ BeSmart.Domain/DTOs/Membership/MembershipDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BeSmart.Domain.DTOs.Membership
+{
+    public class MembershipDTO
+    {
+        public int? Id { get; set; }
+        public string? NameCourse { get; set; }
+        public string? Status { get; set; }
+        public int? CountOfThemes { get; set; }
+    }
+}
