e61030fd9dbeb3e9aea86c463981707c16cb9fbe
gfady <dashyk130903@gmail.com>
2023-02-03 01:15:47 +0300

    add initial configuration for elastic stack

--- BeSmart.WebApi/Program.cs
+++ BeSmart.WebApi/Program.cs
@@ -13,10 +13,16 @@ using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using BeSmart.Application;
 using BeSmart.WebApi.Middleware;
-using BeSmart.Persistence.Data;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
 
 
 var builder = WebApplication.CreateBuilder(args);
+
+ConfigureLogging();
+builder.Host.UseSerilog();
+
 var key = SomeOptions.GenerateBytes();
 
 builder.Services.AddControllers()
@@ -132,3 +138,34 @@ app.UseSwaggerUI(options =>
 });
 
 app.Run();
+
+
+
+static void ConfigureLogging()
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile(
+            $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+            optional: true)
+        .Build();
+
+    Log.Logger = new LoggerConfiguration()
+        .Enrich.FromLogContext()
+        .WriteTo.Debug()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+        .Enrich.WithProperty("Environment", environment)
+        .ReadFrom.Configuration(configuration)
+        .CreateLogger();
+}
+
+ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+{
+    return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+    };
+}
